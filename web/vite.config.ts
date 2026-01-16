3d4e5919918a09fcf19caa3cea7a46e9209f6c74
tye <tye@novatasolutions.com.au>
2026-01-16 10:40:35 +0800

    feat: install tanstack query

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -6,6 +6,9 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue(), tailwindcss()],
+  optimizeDeps: {
+    exclude: ["@tanstack/vue-query"],
+  },
   server: {
     port: 5173,
     hmr: {
